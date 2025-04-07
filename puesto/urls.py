b5ca1fc5565cc13e6184d0aefd69a789b7584318
Luis Mario Suárez <luismariosuarez@lumace.cloud>
2025-04-07 17:48:00 -0600

    Implementación de vistas y plantillas para la gestión de puestos: se añadieron las vistas de detalle y eliminación, junto con sus respectivas plantillas. Se actualizó la configuración de URLs para incluir estas nuevas funcionalidades.

--- puesto/urls.py
+++ puesto/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PuestoListView, PuestoCreateView
+from .views import PuestoListView, PuestoCreateView, PuestoDetailView, PuestoDeleteView
 
 urlpatterns = [
     path('', PuestoListView.as_view(), name='puesto_list'),  # Listado de puestos
     path('nuevo/', PuestoCreateView.as_view(), name='puesto_create'),  # Crear nuevo puesto
+    path('<int:pk>/', PuestoDetailView.as_view(), name='puesto_detail'),  # Detalle de puesto
+    path('eliminar/<int:pk>/', PuestoDeleteView.as_view(), name='puesto_delete'),  # Eliminar puesto
 ]
--- puesto/views.py
+++ puesto/views.py
@@ -1,6 +1,6 @@
 # Django
-from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.contrib import messages
 # Vistas
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 # Modelos
@@ -29,3 +29,20 @@ class PuestoCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
         form.instance.created_by = self.request.user
         form.instance.updated_by = self.request.user
         return super().form_valid(form)
+
+class PuestoDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    permission_required = 'puesto.view_puestomodel'
+    model = PuestoModel
+    template_name = 'puesto_detail.html'
+    context_object_name = 'puesto'
+
+class PuestoDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    permission_required = 'puesto.delete_puestomodel'
+    model = PuestoModel
+    context_object_name = 'puesto'
+    template_name = 'puesto_confirm_delete.html'
+    success_url = reverse_lazy('puesto_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, "Puesto eliminado correctamente.")
+        return super().delete(request, *args, **kwargs)
