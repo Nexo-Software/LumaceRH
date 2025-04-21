9a8f5ad8c0d40af1c008c2f3c6b97a3d9b47cd1b
Luis Mario Suárez <luismariosuarez@lumace.cloud>
2025-04-21 12:38:46 -0600

    Agregar importaciones de vistas y modelos en views.py para gestionar incidencias de empleados.

--- incidencia/views.py
+++ incidencia/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+# Vistas
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+# Modelos
+from .models import CategoriaIncidenciasModel, TablaCalculosIncidenciasModel, TipoIncidenciaModel, IncidenciasEmpleadoModel
+# Mixins
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 # Create your views here.
+
+# Ver incidencias empleados
+
+# Crear una incidencia
+
+# Actualizar una incidencia
