cd330bd4ea8999aeef9cdd729f6e6c939de09a15
Luis Mario Suárez <luismariosuarez@lumace.cloud>
2025-04-22 12:50:27 -0600

    Registrar modelos adicionales en el admin y mejorar la gestión de auditoría en los formularios.

--- incidencia/admin.py
+++ incidencia/admin.py
@@ -13,4 +13,117 @@ class CategoriaIncidenciasAdmin(ModelAdmin):
     list_per_page = 10
     list_editable = ('descripcion',)
     list_display_links = ('nombre',)
+    fieldsets = (
+        (None, {
+            'fields': ('nombre', 'descripcion')
+        }),
+        ('Auditoria', {
+            'fields': ('created_at', 'updated_at', 'created_by', 'updated_by'),
+            'classes': ('collapse',)
+        })
+        )
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by')
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+            obj.updated_by = request.user
+        else:
+            obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+@admin.register(TiposFormulasModel)
+class TiposFormulasAdmin(ModelAdmin):
+    """Admin para los tipos de formulas"""
+    list_display = ('nombre', 'descripcion')
+    search_fields = ('nombre',)
+    list_filter = ('nombre',)
+    ordering = ('nombre',)
+    list_per_page = 10
+    list_editable = ('descripcion',)
+    list_display_links = ('nombre',)
+    fieldsets = (
+        (None, {
+            'fields': ('nombre', 'descripcion')
+        }),
+        ('Auditoria', {
+            'fields': ('created_at', 'updated_at', 'created_by', 'updated_by'),
+            'classes': ('collapse',)
+        })
+        )
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by')
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+            obj.updated_by = request.user
+        else:
+            obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+@admin.register(FormulaIncidenciasModel)
+class FormulaIncidenciasAdmin(ModelAdmin):
+    """Admin para las formulas de incidencias"""
+    list_display = ('nombre', 'descripcion', 'codigo')
+    search_fields = ('nombre',)
+    list_filter = ('nombre',)
+    ordering = ('nombre',)
+    list_per_page = 10
+    list_editable = ('descripcion', 'codigo')
+    list_display_links = ('nombre',)
+    fieldsets = (
+        (None, {
+            'fields': ('nombre', 'descripcion', 'codigo', 'formula_detalle')
+        }),
+        ('Auditoria', {
+            'fields': ('created_at', 'updated_at', 'created_by', 'updated_by'),
+            'classes': ('collapse',)
+        })
+        )
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by')
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+            obj.updated_by = request.user
+        else:
+            obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+@admin.register(TipoIncidenciasModel)
+class TipoIncidenciasAdmin(ModelAdmin):
+    """Admin para los tipos de incidencias"""
+    list_display = ('nombre', 'descripcion', 'categoria', 'formula')
+    search_fields = ('nombre',)
+    list_filter = ('nombre',)
+    ordering = ('nombre',)
+    list_per_page = 10
+    list_editable = ('descripcion', 'categoria', 'formula')
+    list_display_links = ('nombre',)
+    fieldsets = (
+        (None, {
+            'fields': ('nombre', 'descripcion', 'categoria', 'formula')
+        }),
+        ('Auditoria', {
+            'fields': ('created_at', 'updated_at', 'created_by', 'updated_by'),
+            'classes': ('collapse',)
+        })
+        )
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by')
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+            obj.updated_by = request.user
+        else:
+            obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+@admin.register(IncidenciasEmpleados)
+class IncidenciasEmpleadosAdmin(ModelAdmin):
+    """Admin para las incidencias de los empleados"""
     
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by')
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+            obj.updated_by = request.user
+        else:
+            obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
