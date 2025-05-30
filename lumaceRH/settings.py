b43cd757bd2339f2f698ea5df0f1d7075b8292f1
Luis Mario Suárez <luismariosuarez@lumace.cloud>
2025-05-29 23:46:46 -0600

    Add search functionality and custom actions in admin for IncidenciasEmpleados; update requirements for import-export support

--- lumaceRH/settings.py
+++ lumaceRH/settings.py
@@ -184,8 +184,8 @@ AUTHENTICATION_BACKENDS = [
 
 SOCIALACCOUNT_PROVIDERS = {}
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
