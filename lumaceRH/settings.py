6c07d0e5011ae7bcea91d6ce345bd38e0b1477d7
Luis Mario Suárez <luismariosuarez@lumace.cloud>
2025-05-30 00:24:29 -0600

    Refactor email backend configuration to use environment variables for sensitive data

--- lumaceRH/settings.py
+++ lumaceRH/settings.py
@@ -184,13 +184,12 @@ AUTHENTICATION_BACKENDS = [
 
 SOCIALACCOUNT_PROVIDERS = {}
 
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'lmcervantessuarez@gmail.com'
-EMAIL_HOST_PASSWORD = 'gzlf eqat nsxy jpzv'
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 
 # Registro con Email
 ACCOUNT_LOGIN_METHODS = ['email']
