0876bb5c84b0b706d581f0eae14648e536f31d5d
Luis Mario Suárez <luismariosuarez@lumace.cloud>
2025-06-21 23:22:07 -0600

    Implement bulk approval and rejection for incidents in the incidencias view

--- incidencia/views.py
+++ incidencia/views.py
@@ -1,6 +1,8 @@
 # Vistas
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, DeleteView
 from django.views.generic.edit import UpdateView
+from pyexpat.errors import messages
+
 # Modelos
 from .models import IncidenciasEmpleados
 from sucursal.models import SucursalModel
@@ -8,17 +10,20 @@ from sucursal.models import SucursalModel
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 # Shortcuts
 from django.shortcuts import redirect, get_object_or_404
-from django.urls import reverse , reverse_lazy
+from django.urls import reverse, reverse_lazy
 # Formularios
 from .forms import ObservacionesForm
+
+
 # Create your views here.
 
-# Modificar incidencias
+# Modificar incidencias (1x1)
 class EstadoIncidenciaUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = IncidenciasEmpleados
     permission_required = 'incidencia.change_incidenciasempleados'
+
     def post(self, request, *args, **kwargs):
-        incidencia = get_object_or_404(IncidenciasEmpleados, pk=self.kwargs['pk']) # Obtener la incidencia por ID
+        incidencia = get_object_or_404(IncidenciasEmpleados, pk=self.kwargs['pk'])  # Obtener la incidencia por ID
         print(f'La incidencia es: {incidencia}')
         estado = request.POST.get('estado')
         print(f'El estado es: {estado}')
@@ -28,6 +33,32 @@ class EstadoIncidenciaUpdateView(LoginRequiredMixin, PermissionRequiredMixin, Up
             incidencia.save()
         return redirect(request.META.get('HTTP_REFERER', reverse('dashboard')))
 
+
+# Modificar incidencias (general)
+class EstadoIncdenciasGeneralUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    model = IncidenciasEmpleados
+    permission_required = 'incidencia.change_incidenciasempleados'
+
+    def post(self, request, *args, **kwargs):
+        incidencias_ids = request.POST.getlist('lista_incidencias')
+        accion = request.POST.get('accion')
+
+        # Obtener todas las incidencias en una sola consulta
+        incidencias = IncidenciasEmpleados.objects.filter(id__in=incidencias_ids)
+
+        if accion == 'APROBAR':
+            for incidencia in incidencias:
+                incidencia.estado_incidencia = 'APROBADA'
+                incidencia.save()  # ¡Activa la señal post_save!
+
+        elif accion == 'RECHAZAR':
+            for incidencia in incidencias:
+                incidencia.estado_incidencia = 'RECHAZADA'
+                incidencia.save()  # ¡Activa la señal post_save!
+
+        return redirect('dashboard')
+
+
 class IncidenciasGeneralListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = IncidenciasEmpleados
     permission_required = 'incidencia.view_incidenciasempleados'
@@ -35,19 +66,23 @@ class IncidenciasGeneralListView(LoginRequiredMixin, PermissionRequiredMixin, Li
     context_object_name = 'incidencias'
 
     def get_queryset(self):
-        return IncidenciasEmpleados.objects.filter(estado_incidencia='PENDIENTE').order_by('-fecha')[:10]  # Limitamos a las 10 últimas incidencias pendientes
+        return IncidenciasEmpleados.objects.filter(estado_incidencia='PENDIENTE').order_by('-fecha')[
+               :10]  # Limitamos a las 10 últimas incidencias pendientes
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['incidencias_count'] = self.get_queryset().count()
         context['sucursales'] = SucursalModel.objects.all()
-        context['col'] = int(12/len(context['sucursales']) if context['sucursales'] else 12)
+        context['col'] = int(12 / len(context['sucursales']) if context['sucursales'] else 12)
         return context
+
 
 class IncidenciasSucursalListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = IncidenciasEmpleados
     permission_required = 'incidencia.view_incidenciasempleados'
     template_name = 'incidencias_sucursal.html'
     context_object_name = 'incidencias'
+
     # Contexto adicional para la plantilla
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -55,6 +90,7 @@ class IncidenciasSucursalListView(LoginRequiredMixin, PermissionRequiredMixin, L
         context['sucursal'] = get_object_or_404(SucursalModel, pk=self.kwargs['pk'])
         context['incidencias_count'] = self.get_queryset().count()
         return context
+
     # Obtener los empleados de una sucursal específica y de ellos, buscamos sus incidencias
     def get_queryset(self):
         sucursal_id = self.kwargs.get('pk')
@@ -63,9 +99,10 @@ class IncidenciasSucursalListView(LoginRequiredMixin, PermissionRequiredMixin, L
             estado_incidencia='PENDIENTE'
         ).order_by('-fecha')[:10]  # Limitamos a las 10 últimas incidencias pendientes
 
+
 class IncidenciaUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = IncidenciasEmpleados
     template_name = 'editar_incidencia.html'
     permission_required = 'incidencia.editar_incidencias'
     form_class = ObservacionesForm
-    success_url = reverse_lazy('incidencias-general-list') # regresar a la lista de incidencias generales
+    success_url = reverse_lazy('incidencias-general-list')  # regresar a la lista de incidencias generales
